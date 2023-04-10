53edff569874196466e85ae8476998361b76a711
AdarshJoshi07 <130400299+AdarshJoshi07@users.noreply.github.com>
2023-04-11 00:50:57 +0530

    Update fahtoCelMain.java

--- fahtoCelMain.java
+++ fahtoCelMain.java
@@ -1,34 +1,41 @@
-abstract class Temperature
+abstract class Temperature1
 {
 	double temp;
-	abstract void changeTemp();
 	void setTempData(double data)
 	{
 		temp = data;
 	}
+	abstract void changeTemp();
 }
-
-class Fahrenheit extends Temperature
+class Fahrenheit1 extends Temperature1
 {
 	double ctemp;
 	public void changeTemp()
 	{
-		ctemp = (5/9.0) * (temp - 32);
-		System.out.println("the temperature in celsius is:" + ctemp + " degrees");
+		ctemp = (5.0/9.0)*(temp - 32);
+		System.out.println("the temperature in celsius is: " + ctemp + " degrees");
 		
 	}
 }
 
-
-
-
-public class temp_conversionMain {
+class Celsius extends Temperature1
+{
+	double ftemp;
+	public void changeTemp()
+	{
+		ftemp = temp* (9.0 / 5.0) + 32;
+		System.out.println("the temperature in fahrenheit is:" + ftemp + " degrees");
+		
+	}
+}
+public class fahToCelsiusMain {
 	public static void main(String[] args)
 	{
-		Fahrenheit obj = new Fahrenheit();
-//		obj.setTempData(32);
-//		obj.changeTemp();
-		obj.setTempData(98.6);
-		obj.changeTemp();
+		Celsius c = new Celsius();
+		c.setTempData(0);
+		c.changeTemp();
+		Fahrenheit1 f = new Fahrenheit1();
+		f.setTempData(32);
+		f.changeTemp();
 	}
 }
